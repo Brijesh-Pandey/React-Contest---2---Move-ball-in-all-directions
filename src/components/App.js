9859155206ff33c80c11d152361e0208d74e9359
Brijesh Pandey <30575594+Brijesh-Pandey@users.noreply.github.com>
2020-10-25 18:39:51 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -5,18 +5,34 @@ const App = () => {
   const [renderBall, setRenderBall] = useState(false);
   const [x, setX] = useState(0);
   const [y, setY] = useState(0);
-  const [ballPosition,setBallPosition] = useState({
+  const [ballPosition, setBallPosition] = useState({
     left: "0px",
-    top: "0px",
+    top: "0px"
   });
-  const reset = () => {};
-  const renderChoice = () => {};
+  const reset = () => {
+    setX("0px");
+    setY("0px");
+  };
+  const start = () => {
+    setRenderBall(true);
+  };
+  const renderChoice = () => {
+    if (renderBall) {
+      return <div className="ball" style={ballPosition}></div>;
+    } else
+      return (
+        <button onClick={start} className="start">
+          Start
+        </button>
+      );
+  };
 
   return (
     <div className="playground">
       <button onClick={reset} className="reset">
         Reset
       </button>
+
       {renderChoice()}
     </div>
   );
